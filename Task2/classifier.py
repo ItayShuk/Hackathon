330a79b7000bb027fb85a8741c0133bd70370809
ItayShuk <itay.shukrun@mail.huji.ac.il>
2021-06-02 21:44:33 +0300

    first

--- Task2/classifier.py
+++ Task2/classifier.py
@@ -1,4 +1,6 @@
-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 crimes_dict = {0: 'BATTERY', 1: 'THEFT', 2: 'CRIMINAL DAMAGE', 3: 'DECEPTIVE PRACTICE', 4: 'ASSAULT'}
 
@@ -8,8 +10,15 @@ def predict(X):
 def send_police_cars(X):
     pass
 
-def load():
+def load(path):
     df = pd.read_csv(path)
     # print(df.shape)
     df = df.dropna()
 
+def main():
+    load()
+
+
+
+if __name__ == '__main__':
+    main()
