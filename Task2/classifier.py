bd20a2b30ac3b69215c7f3a185fc7c39c84b1686
ItayShuk <itay.shukrun@mail.huji.ac.il>
2021-06-02 21:35:58 +0300

    first

---
+++ Task2/classifier.py
@@ -0,0 +1,15 @@
+
+
+crimes_dict = {0: 'BATTERY', 1: 'THEFT', 2: 'CRIMINAL DAMAGE', 3: 'DECEPTIVE PRACTICE', 4: 'ASSAULT'}
+
+def predict(X):
+    pass
+
+def send_police_cars(X):
+    pass
+
+def load():
+    df = pd.read_csv(path)
+    # print(df.shape)
+    df = df.dropna()
+
